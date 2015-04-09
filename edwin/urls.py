0fcaa31ad8e8834f3b5137daad177e024faa815b
Mike Cooper <mythmon@gmail.com>
2015-04-09 13:09:48 -0700

    Add debug static urls.

--- edwin/urls.py
+++ edwin/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 
 urlpatterns = [
@@ -6,3 +8,6 @@ urlpatterns = [
     url(r'admin', include(admin.site.urls)),
     url(r'', include('edwin.client.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
