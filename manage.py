2a78d94e507ff165556fc4c13d43dc3f8ef67554
Mike Cooper <mythmon@gmail.com>
2015-05-08 14:41:48 -0700

    Make default configuration "Dev" in manage.py.
    Fixes #17

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edwin.settings")
-    os.environ.setdefault("DJANGO_CONFIGURATION", "Base")
+    os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
 
     from configurations.management import execute_from_command_line
 
