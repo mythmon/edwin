6c76dd8d3af99e6679b7013156f2db0ac9d1f65b
Will Kahn-Greene <willkg@mozilla.com>
2015-05-15 15:39:37 -0400

    Change to show only the gravatar
    Fixes #32

--- edwin/client/static/js/components/Timeline.js
+++ edwin/client/static/js/components/Timeline.js
@@ -152,7 +152,7 @@ class BugRow extends React.Component {
 
 
 /**
- * Renders a small chip for a user. Shows a name and a Gravatar.
+ * Renders a small chip for a user. Shows a Gravatar.
  *
  * TODO: This is probably useful enough to move somewhere public
  *
@@ -167,13 +167,11 @@ class AssignedTo extends React.Component {
       return (
         <span className="AssignedTo">
           <span className="AssignedTo__avatar--empty"/>
-          <span className="AssignedTo__name">—</span>
         </span>
       );
     } else {
       return <span className="AssignedTo">
         <Gravatar className="AssignedTo__avatar" email={user.get('email')} https size={36}/>
-        <span className="AssignedTo__name">{user.get('real_name', user.get('name'))}</span>
       </span>;
     }
   }
