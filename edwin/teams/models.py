8cc302b116e04d0254c10a079fc72ccdb5ef22cf
Will Kahn-Greene <willkg@mozilla.com>
2015-05-15 15:26:48 -0400

    Enforce uniqueness on slug

--- edwin/teams/models.py
+++ edwin/teams/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Team(models.Model):
     name = models.CharField(max_length=64)
-    slug = models.SlugField(max_length=64)
+    slug = models.SlugField(max_length=64, unique=True)
     # Burn rate is points per week
     current_burn_rate = models.IntegerField()
     # Like "mozilla/edwin"
