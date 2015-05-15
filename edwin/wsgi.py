15d9cb3b07cdab18c4994ff4ef882a9fc28be04c
Will Kahn-Greene <willkg@mozilla.com>
2015-05-15 13:37:40 -0400

    Fix wsgi.py to set defaults first
    We need to set default environ stuff before importing django-y things
    otherwise they don't have any effect.

--- edwin/wsgi.py
+++ edwin/wsgi.py
@@ -1,9 +1,10 @@
 import os
 
-from configurations.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
+# These need to be set before we start pulling in Django things.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edwin.settings")
 os.environ.setdefault("DJANGO_CONFIGURATION", "Base")
+
+from configurations.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 application = DjangoWhiteNoise(get_wsgi_application())
