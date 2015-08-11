7a70d230d3ceb3c37d718f138e80b132b9a05fae
Mike Cooper <mythmon@gmail.com>
2015-08-11 15:36:43 -0700

    Add migration for multiple repos per team.

---
+++ edwin/teams/migrations/0005_auto_20150811_2236.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('teams', '0004_auto_20150516_0009'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='team',
+            name='github_repo',
+            field=models.CharField(blank=True, help_text='Comma-separated list of repos, like "mozilla/edwin,mozilla/edwin2"', max_length=1024),
+        ),
+    ]
